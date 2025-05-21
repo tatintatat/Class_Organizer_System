b80aede21e7c758b5acf040227848102e90e7b84
tatintatat <163250173+tatintatat@users.noreply.github.com>
2025-05-22 05:05:48 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function showChoicePage() {
 }
 
 function chooseRole(role) {
-    if (role === 'Student')
+    if (role === 'Student') ///STUDENTS PAGE
     {
         document.getElementById('choicePage').style.display = 'none';
         document.getElementById('STdashboardPage').style.display = 'flex';
@@ -33,28 +33,74 @@ function chooseRole(role) {
         document.getElementById('STdashboardPage').style.justifyContent = 'center';
         document.getElementById('teacherpage').style.display =  'none';
         document.getElementById('schedule-studentpage').style.display = "none";
+        document.getElementById('more').style.display = "none";
         
     }
-    else if (role === 'Teacher')
+    else if (role === 'Teacher')  ///TEACHERS PAGE
     {
         document.getElementById('choicePage').style.display = 'none';
+        document.getElementById('Teacher-Page').style.display = 'flex';
+        document.getElementById('Teacher-Page').style.flexDirection = 'column';
+        document.getElementById('Teacher-Page').style.alignItems = 'center';
+        document.getElementById('Teacher-Page').style.justifyContent = 'center';
+        document.getElementById('Secondpage').style.display = "none";
+        document.getElementById('mores').style.display = "none";
+        document.getElementById('schedule-studentpage').style.display = "none";
+        document.getElementById('Secondpage').style.display = "none";
+        
     }
 }
-function Teachers()
+function Teachers() /// TEACHER FOR STUDENTS PAGE
 {
   document.getElementById('homepage').style.display = 'none';
   document.getElementById('teacherpage').style.display =  'block';
   document.getElementById('schedule-studentpage').style.display = "none";
 }
-function home()
+function home() /// HOMEPAGE FOR STUDENTS PAGE
 {
     document.getElementById('teacherpage').style.display = 'none';
     document.getElementById('homepage').style.display = 'block';
     document.getElementById('schedule-studentpage').style.display = "none";
 }
-function schedulestudent()
+function schedulestudent() /// SCHEDULE FOR STUDENTS PAGE
 {
     document.getElementById('schedule-studentpage').style.display = "block";
     document.getElementById('homepage').style.display = 'none';
     document.getElementById('teacherpage').style.display = 'none';
+}
+function more() ///THREE BARS
+{ 
+  const moreElement = document.getElementById('more');
+  
+  if (moreElement.style.display === 'none' || moreElement.style.display === '') {
+    moreElement.style.display = 'block';
+  } else {
+    moreElement.style.display = 'none';
+  }
+}
+function logout()
+{
+    document.getElementById('loginPage').style.display = 'block';
+    document.getElementById('Teacher-Page').style.display = 'none';
+    document.getElementById('STdashboardPage').style.display = 'none';
+}
+/// FOR TEACHERS MAINPAGE
+function secondpage()
+{
+    document.getElementById('Secondpage').style.display = "block";
+    document.getElementById('Mainpage').style.display = "none";
+}
+function mainpage()
+{
+    document.getElementById('Secondpage').style.display = "none";
+    document.getElementById('Mainpage').style.display = "block";
+}
+function bars()
+{
+    const moresElement = document.getElementById('mores');
+    if (moresElement.style.display === 'none' || moresElement.style.display === '') {
+    moresElement.style.display = 'block';
+    } else {
+    moresElement.style.display = 'none';
+    }
 }
